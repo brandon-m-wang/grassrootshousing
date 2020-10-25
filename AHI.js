a6e4d3d9ac8e3561b380b58ccdb4d20c6a92f5e6
zanenasrallah <71055457+zanenasrallah@users.noreply.github.com>
2020-10-24 20:11:09 -0500

    Updated page navigation, and connected back-end Json imnfomation to front-end Google Map instances
    Added naviagation back to the home page from all the subpages, and made it so that the google map instances on each subpage has visual indicators of the information stored on our Azure server

---
+++ AHI.js
@@ -0,0 +1,142 @@
+var map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 12,
+        center: {lat: 30.2672, lng: -97.7431},
+        styles: [
+            {elementType: "geometry", stylers: [{color: "#242f3e"}]},
+            {elementType: "labels.text.stroke", stylers: [{color: "#242f3e"}]},
+            {elementType: "labels.text.fill", stylers: [{color: "#746855"}]},
+            {
+                featureType: "administrative.locality",
+                elementType: "labels.text.fill",
+                stylers: [{color: "#d59563"}],
+            },
+            {
+                featureType: "poi",
+                elementType: "labels.text.fill",
+                stylers: [{color: "#d59563"}],
+            },
+            {
+                featureType: "poi.park",
+                elementType: "geometry",
+                stylers: [{color: "#263c3f"}],
+            },
+            {
+                featureType: "poi.park",
+                elementType: "labels.text.fill",
+                stylers: [{color: "#6b9a76"}],
+            },
+            {
+                featureType: "road",
+                elementType: "geometry",
+                stylers: [{color: "#38414e"}],
+            },
+            {
+                featureType: "road",
+                elementType: "geometry.stroke",
+                stylers: [{color: "#212a37"}],
+            },
+            {
+                featureType: "road",
+                elementType: "labels.text.fill",
+                stylers: [{color: "#9ca5b3"}],
+            },
+            {
+                featureType: "road.highway",
+                elementType: "geometry",
+                stylers: [{color: "#746855"}],
+            },
+            {
+                featureType: "road.highway",
+                elementType: "geometry.stroke",
+                stylers: [{color: "#1f2835"}],
+            },
+            {
+                featureType: "road.highway",
+                elementType: "labels.text.fill",
+                stylers: [{color: "#f3d19c"}],
+            },
+            {
+                featureType: "transit",
+                elementType: "geometry",
+                stylers: [{color: "#2f3948"}],
+            },
+            {
+                featureType: "transit.station",
+                elementType: "labels.text.fill",
+                stylers: [{color: "#d59563"}],
+            },
+            {
+                featureType: "water",
+                elementType: "geometry",
+                stylers: [{color: "#17263c"}],
+            },
+            {
+                featureType: "water",
+                elementType: "labels.text.fill",
+                stylers: [{color: "#515c6d"}],
+            },
+            {
+                featureType: "water",
+                elementType: "labels.text.stroke",
+                stylers: [{color: "#17263c"}],
+            },
+        ],
+    });
+}
+
+let circleArray = []
+
+
+fetch('https://grassrootsforfamiliesserver.azurewebsites.net/get_all_affordable_housing')
+    .then(response => response.json())
+    .then(data => drawCircles(data["affordable_housing"]));
+
+
+function drawCircles(data) {
+    for (key in data) {
+        console.log(data[key])
+        latlngobj = {lat: parseFloat(data[key]["latitude"]), lng: parseFloat(data[key]["longitude"])}
+        const circle = new google.maps.Circle({
+            fillColor: '#e29002',
+            fillOpacity: .5,
+            strokeColor: 'white',
+            strokeWeight: .5,
+            clickable: true,
+            map,
+            center: latlngobj,
+            radius:150
+        })
+        let contentString = "<strong>Project Name</strong>: " + data[key]["project_name"] + "<br><b>Ownership:</b> " + data[key]["Ownership"] + "<br><b>Address:</b> " + data[key]["address"] + "<br><b>Unit Type:</b> " + data[key]["unit_type"]
+        circle.setMap(map)
+        let infowindow = new google.maps.InfoWindow({
+            content: contentString
+        });
+        google.maps.event.addListener(circle, 'click', function (ev) {
+            infowindow.close(map);
+            infowindow.setPosition(circle.getCenter());
+            infowindow.open(map);
+        });
+    };
+}
+
+
+$(function () {
+    $("#submission").click(function (event) {
+        submitUserData()
+        event.preventDefault()
+    });
+});
+
+function submitUserData() {
+    raceSelection = $('#race option:selected').text();
+    genderSelection = $('#gender option:selected').text();
+    sexualitySelection = $('#sexuality option:selected').text();
+    religionSelection = $('#religion option:selected').text();
+
+    fetch(`https://us-central1-safeguard-292111.cloudfunctions.net/getLocations?race=${raceSelection}&religion=${genderSelection}&sexuality=${sexualitySelection}&gender=${religionSelection}`)
+        .then(response => response.json())
+        .then(data => drawCircles(data[0]));
+}
--- mapRepurposing.js
+++ mapRepurposing.js
@@ -89,82 +89,36 @@ function initMap() {
 
 let circleArray = []
 
-let contentString = "Location: South Park Date: 8/31/2020 Description: Anti-Black assault on multiple local community members. In violation of state (RCW 9A.36.080), and municipal code (SMC 12A.06.115)."
 
-fetch('https://us-central1-safeguard-292111.cloudfunctions.net/getAllCrimes')
+fetch('https://grassrootsforfamiliesserver.azurewebsites.net/get_all_office_spaces')
     .then(response => response.json())
-    .then(data => drawCircles(data[0]));
-
+    .then(data => drawCircles(data["office_spaces"]));
 
 function drawCircles(data) {
-    for (let i in circleArray) {
-        circleArray[i].setMap(null)
-    }
-    circleArray = []
     for (key in data) {
-        latlngobj = key.split(", ")
-        latlngobj = {lat: parseFloat(latlngobj[0]), lng: parseFloat(latlngobj[1])}
-        if (data[key] < 10) {
-            const circle = new google.maps.Circle({
-                fillColor: '#e29002',
-                fillOpacity: .8,
-                strokeColor: 'white',
-                strokeWeight: .5,
-                clickable: true,
-                map,
-                center: latlngobj,
-                radius: Math.sqrt(data[key]) * 50
-            })
-            circleArray.push(circle)
-            let infowindow = new google.maps.InfoWindow({
-                content: contentString
-            });
-            google.maps.event.addListener(circle, 'click', function (ev) {
-                infowindow.setPosition(circle.getCenter());
-                infowindow.open(map);
-            });
-        } else if (data[key] < 50) {
-            const circle = new google.maps.Circle({
-                fillColor: '#ca3900',
-                fillOpacity: .8,
-                strokeColor: 'white',
-                strokeWeight: .5,
-                clickable: true,
-                map,
-                center: latlngobj,
-                radius: Math.sqrt(data[key]) * 50
-            })
-            circleArray.push(circle)
-            let infowindow = new google.maps.InfoWindow({
-                content: contentString
-            });
-            google.maps.event.addListener(circle, 'click', function (ev) {
-                infowindow.setPosition(circle.getCenter());
-                infowindow.open(map);
-            });
-        } else {
-            const circle = new google.maps.Circle({
-                fillColor: '#d00000',
-                fillOpacity: .8,
-                strokeColor: 'white',
-                strokeWeight: .5,
-                clickable: true,
-                map,
-                center: latlngobj,
-                radius: Math.sqrt(data[key]) * 50
-            })
-            circleArray.push(circle)
-            let infowindow = new google.maps.InfoWindow({
-                content: contentString
-            });
-            google.maps.event.addListener(circle, 'click', function (ev) {
-                infowindow.setPosition(circle.getCenter());
-                infowindow.open(map);
-            });
-        }
-
-    }
-    ;
+        console.log(data[key])
+        latlngobj = {lat: parseFloat(data[key]["latitude"]), lng: parseFloat(data[key]["longitude"])}
+        const circle = new google.maps.Circle({
+            fillColor: '#C0C0C0',
+            fillOpacity: .7,
+            strokeColor: 'white',
+            strokeWeight: .5,
+            clickable: true,
+            map,
+            center: latlngobj,
+            radius:200
+        })
+        let contentString = "<strong>Name</strong>: " + data[key]["name"]
+        circle.setMap(map)
+        let infowindow = new google.maps.InfoWindow({
+            content: contentString
+        });
+        google.maps.event.addListener(circle, 'click', function (ev) {
+            infowindow.close(map);
+            infowindow.setPosition(circle.getCenter());
+            infowindow.open(map);
+        });
+    };
 }
 
 
@@ -185,54 +139,3 @@ function submitUserData() {
         .then(response => response.json())
         .then(data => drawCircles(data[0]));
 }
-
-
-/*  NO LONGER RELEVANT ------
-          // Create a <script> tag and set the USGS URL as the source.
-          var script = document.createElement('script');
-
-          // // This example uses a local copy of the GeoJSON stored at
-          // http:earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-          script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
-*/
-//loading data using JSON file
-// map.data.loadGeoJson("data.JSON");
-
-/*         document.getElementsByTagName('head')[0].appendChild(script);
-
-        map.data.setStyle(function(feature) {
-          var magnitude = feature.getProperty('mag');
-          return {
-            icon: getCircle(magnitude)
-          };
-        });
-
-      }
-
-      function getCircle(magnitude) {
-        return {
-          path: google.maps.SymbolPath.CIRCLE,
-          fillColor: '#e27500',
-          fillOpacity: .2,
-          scale: Math.pow(2, magnitude) / 2,
-          strokeColor: 'white',
-          strokeWeight: .5
-        };
-      }
-
-      function eqfeed_callback(results) {
-        map.data.addGeoJson(results);
-      }
-*/
-// const infowindow = new google.maps.InfoWindow({
-//   content: 'contentString'
-// });
-
-// const marker = new google.maps.Marker({
-//   position: uluru,
-//   map,
-//   title: "Uluru (Ayers Rock)",
-// });
-// marker.addListener("click", () => {
-//   infowindow.open(map, marker);
-// });
